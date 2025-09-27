77de6aac85588b4c46580032217f59b41cc1a557
Roshitha-07 <mettaroshitha@gmail.com>
2025-09-27 22:07:42 +0530

    delete students

--- Student_management/main.py
+++ Student_management/main.py
@@ -1,5 +1,5 @@
-from  add_student import add_student
-# from clerk.delete_student import delete_student
+from clerk.add_student import add_student
+from clerk.delete_student import delete_student
 from faculty.search_student import search_student
 from faculty.update_student import update_student
 # from hod.summary_reports import generate_summary
